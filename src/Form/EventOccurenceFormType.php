6fc450365e50c740d085ed3def5702c4adede6ea
Vitek <VitmT@matuvi.com>
2023-05-18 10:09:51 +0200

    uprava casoveho udaje

--- src/Form/EventOccurenceFormType.php
+++ src/Form/EventOccurenceFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 
 class EventOccurenceFormType extends AbstractType
 {
@@ -23,12 +24,17 @@ class EventOccurenceFormType extends AbstractType
             ->add('eventStart', TimeType::class, [
                 'attr' => ['class' => 'save dateSelect'],
             ]);
-        $builder
-            ->add('eventEnd', TimeType::class, [
+            $builder->add('Duration', DateIntervalType::class, [
+                'widget'      => 'choice',
+               'with_years'  => false,
+                'with_months' => false,
+                'with_days' => false,
+                'with_minutes'   => true,
+                'with_hours'  => true,
                 'attr' => ['class' => 'save dateSelect'],
+                'minutes' => [0 => 0, 15 => 15, 30 => 30, 45 => 45],
+                'hours' => array_combine(range(0, 5), range(0, 5)),
             ]);
-        $builder
-            ->add('eventPlace');
         $builder
             ->add('save', SubmitType::class, [
                 'attr' => ['class' => 'save btn btn-success'],
