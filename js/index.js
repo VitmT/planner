2bbf357941d37466bc5e12a1795b3f87a05b1615
miraksandak <ksandak.cz@gmail.com>
2023-05-18 15:31:00 +0200

    set minTime and maxTime in timepicker

--- js/index.js
+++ js/index.js
@@ -7,7 +7,17 @@ $(document).ready(function(){
         interval: 30,
         dynamic: false,
         dropdown: true,
-        scrollbar: true
+        scrollbar: true,
+    });
+
+    $('.timepicker2').timepicker({
+        timeFormat: 'HH:mm',
+        interval: 30,
+        dynamic: false,
+        dropdown: true,
+        scrollbar: true,
+        minTime: '00:00',
+        maxTime: '05:00'
     });
 
 })
