3754f44c89ca4fffadfeef1c7bcd467589785c82
DominikKosar <dom707@seznam.cz>
2023-05-18 14:21:14 +0200

    Předělání tabulky a smazání indextype.php

--- pages/index.php
+++ pages/index.php
@@ -4,5 +4,11 @@ use App\Form\IndexType;
 
 return [
     "template" => "index.html.twig",
-    "form" => $this->createForm(IndexType::class,[]),
+    "events" => [
+        ["editLink" => "#", "place" => "Nové Sedlo", "date" => date('y-m-d'), "time" => date('h-i-s')],
+        ["editLink" => "#", "place" => "Chodov", "date" => date('y-m-d'), "time" => date('h-i-s')],
+        ["editLink" => "#", "place" => "Ostrov", "date" => date('y-m-d'), "time" => date('h-i-s')],
+        ["editLink" => "#", "place" => "Stará Role", "date" => date('y-m-d'), "time" => date('h-i-s')],
+        ["editLink" => "#", "place" => "Sokolov", "date" => date('y-m-d'), "time" => date('h-i-s')],
+    ],
 ];
