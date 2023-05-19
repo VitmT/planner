8f23763d9980d9fa0630ea68b2f0c9ea74183a68
DominikKosar <dom707@seznam.cz>
2023-05-19 08:53:33 +0200

    Smazání sekund v index.php

--- pages/index.php
+++ pages/index.php
@@ -5,10 +5,10 @@ use App\Form\IndexType;
 return [
     "template" => "index.html.twig",
     "events" => [
-        ["editLink" => "#", "place" => "Nové Sedlo", "date" => date('y.m.d'), "time" => date('h:i:s')],
-        ["editLink" => "#", "place" => "Chodov", "date" => date('y.m.d'), "time" => date('h:i:s')],
-        ["editLink" => "#", "place" => "Ostrov", "date" => date('y.m.d'), "time" => date('h:i:s')],
-        ["editLink" => "#", "place" => "Stará Role", "date" => date('y.m.d'), "time" => date('h:i:s')],
-        ["editLink" => "#", "place" => "Sokolov", "date" => date('y.m.d'), "time" => date('h:i:s')],
+        ["editLink" => "#", "place" => "Nové Sedlo", "date" => date('y.m.d'), "time" => date('h:i')],
+        ["editLink" => "#", "place" => "Chodov", "date" => date('y.m.d'), "time" => date('h:i')],
+        ["editLink" => "#", "place" => "Ostrov", "date" => date('y.m.d'), "time" => date('h:i')],
+        ["editLink" => "#", "place" => "Stará Role", "date" => date('y.m.d'), "time" => date('h:i')],
+        ["editLink" => "#", "place" => "Sokolov", "date" => date('y.m.d'), "time" => date('h:i')],
     ],
 ];
