cebb8fc1fc531a2374895bfa3d472113e1eaf175
miraksandak <ksandak.cz@gmail.com>
2023-05-24 13:24:59 +0200

    Map form to data

--- src/Controller/NewEventController.php
+++ src/Controller/NewEventController.php
@@ -8,11 +8,20 @@ use App\Form\EventOccurenceFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\ReccuringEventOccurence as RecurringEventOccurence;
+use App\Repository\ReccuringEventOccurenceRepository;
+use App\Repository\ReccuringEventRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class NewEventController extends AbstractController
 {
-    #[Route('/new-event', name: 'new-event')]
-    public function Login(?int $id, Request $request, EntityManagerInterface $entityManager): Response
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private RequestStack $requestStack
+    ) {
+    }
+
+    #[Route('/new-event', name: 'ushdfaish')]
+    public function login(?int $id, Request $request, EntityManagerInterface $entityManager): Response
     {
         if ($id === null) {
             $recurringEventOccurence = new RecurringEventOccurence();
@@ -21,7 +30,7 @@ class NewEventController extends AbstractController
         }
         //$reccuringEventOccurence = [];//new ReccuringEvent();
                 
-        $form = $this->createForm(EventOccurenceFormType::class);
+        $form = $this->createForm(EventOccurenceFormType::class, $recurringEventOccurence);
 
         $form->handleRequest($request);
 
@@ -31,6 +40,44 @@ class NewEventController extends AbstractController
             $entityManager->persist($reccuringEvent);
             $entityManager->flush();
 
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render('EventOccurenceForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/new/{reccuringEventId}', name: 'new-event')]
+    public function new(
+        int $reccuringEventId,
+        ReccuringEventRepository $reccuringEventRepository
+    ): Response
+    {
+        $reccuringEvent = $reccuringEventRepository->find($reccuringEventId);
+
+        return $this->neco();
+    }
+
+    #[Route('/edit/{id}', name: 'edit-event')]
+    public function edit(
+        int $id,
+        ReccuringEventOccurenceRepository $reccuringEventOccurenceRepository
+    ): Response
+    {
+        $reccuringEventOccurence = $reccuringEventOccurenceRepository->find($id);
+        $reccuringEvent = $reccuringEventOccurence->getReccuringEvent();
+
+        return $this->neco();
+    }
+
+    private function neco(): Response
+    {
+        $request = $this->requestStack->getMainRequest();
+        $form = $this->createForm(EventOccurenceFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
             return $this->redirectToRoute('app_index');
         }
 
--- src/Form/EventOccurenceFormType.php
+++ src/Form/EventOccurenceFormType.php
@@ -2,60 +2,137 @@
 
 namespace App\Form;
 
+use App\Entity\ReccuringEventOccurence;
+use DateInterval;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException as ExceptionUnexpectedTypeException;
+use Symfony\Component\Form\FormInterface;
 
-class EventOccurenceFormType extends AbstractType
+class EventOccurenceFormType extends AbstractType implements DataMapperInterface
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('name', TextType::class, [
-            'attr' => ['class' => 'save form-control mb-3'],
-            ]);
-        $builder
-            ->add('eventDate', DateType::class, [
-                'attr' => [
-                    'class' => 'save form-control d-flex justify-content-center mb-3',
-                ],
-                'widget' => 'single_text',
-                'data' => new \DateTime(),
-            ]);
-        $builder
-            ->add('eventStart', TimeType::class, [
-                'attr' => [
-                    'class' => 'save form-control d-flex justify-content-center text-center mb-3 timepicker',
-                ],
-                'widget' => 'single_text',
-            ]);
-        $builder->add('Duration', DateIntervalType::class, [
-            'with_years'  => false,
-            'with_months' => false,
-            'with_days' => false,
-            'with_minutes'   => true,
-            'with_hours'  => true,
+        $builder->add(
+            'timestamp',
+            DateType::class,
+            [
+            'label' => 'Datum',
             'attr' => [
-                'class' => 'save form-control d-flex justify-content-center text-center mb-3 timepicker2',
+                'class' => 'form-control mb-3',
             ],
             'widget' => 'single_text',
-        ]);
-        $builder
-            ->add('save', SubmitType::class, [
-                'attr' => ['class' => 'save loginButton btn btn-success w-100'],
-            ]);
-        ;
+            'data' => new \DateTime(),
+            ]
+        );
+
+        $builder->add(
+            'startTimeOffset',
+            TimeType::class,
+            [
+            'label' => 'Start Time',
+            'attr' => [
+                'class' => 'form-control mb-3 timepicker',
+            ],
+            'widget' => 'single_text',
+            ]
+        );
+
+        $builder->add(
+            'endTimeOffset',
+            TimeType::class,
+            [
+            'label' => 'End Time',
+            'attr' => [
+                'class' => 'form-control mb-3 timepicker',
+            ],
+            'widget' => 'single_text',
+            ]
+        );
+
+
+        $builder->add(
+            'save',
+            SubmitType::class,
+            [
+                'label' => 'Uložit',
+                'attr' => [
+                    'class' => 'btn btn-success btn-login text-uppercase fw-bold form-control',
+                ],
+            ]
+        );
+        $builder->setDataMapper($this);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => ReccuringEventOccurences::class
-        ]);
+        $resolver->setDefaults(
+            [
+            'data_class' => ReccuringEventOccurence::class,
+            ]
+        );
+    }
+
+    public function mapDataToForms($viewData, iterable $forms): void
+    {
+        // there is no data yet, so nothing to prepopulate
+        if (null === $viewData) {
+            return;
+        }
+
+        // invalid data type
+        if (!$viewData instanceof ReccuringEventOccurence) {
+            throw new ExceptionUnexpectedTypeException($viewData, ReccuringEventOccurence::class);
+        }
+
+        /**
+         * @var FormInterface[] $forms
+        */
+        $forms = iterator_to_array($forms);
+
+        $startDateTime = $viewData->getTimestamp();
+        $duration = $viewData->getDuration();
+        
+        // initialize form field values
+        $forms['timestamp']->setData(new DateTime($startDateTime->format('d-m-Y')));
+        $forms['startTimeOffset']->setData($viewData->getTimestamp());
+        $date = DateTime::createFromInterface(
+            $viewData->getTimestamp()
+        );
+        if ($duration < 0) {
+            $method = 'sub';
+            $duration = -$duration;
+        } else {
+            $method = 'add';
+        }
+        $date->$method(new DateInterval(sprintf('PT%dS', $duration)));
+
+        $forms['endTimeOffset']->setData($date);
+    }
+
+    public function mapFormsToData(iterable $forms, &$viewData): void
+    {
+        /**
+         * @var FormInterface[] $forms
+        */
+        $forms = iterator_to_array($forms);
+
+        $date = $forms['timestamp']->getData();
+        $startTime = $forms['startTimeOffset']->getData();
+        $endTime = $forms['endTimeOffset']->getData();
+
+        $startDateTime = new DateTime($date->format('d-m-Y') . ' ' . $startTime->format('H:i:s'));
+        $viewData->setTimestamp($startDateTime);
+
+        $duration = $endTime->getTimestamp() - $startTime->getTimestamp();
+        $viewData->setDuration($duration);
     }
 }
