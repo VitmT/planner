b46cf5adad816dfbb73a71621cd25a7273bcf24c
Vitek <VitmT@matuvi.com>
2023-05-25 10:53:28 +0200

    Pridani souboru do recurrence slozky

--- src/Recurrence/Recurrence.php
+++ src/Recurrence/Recurrence.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Recurremnce;
+namespace App\Recurrence;
 
 use DateTimeImmutable;
 
-abstract class Recurrence{
-
+abstract class Recurrence
+{
     abstract public function getNextTimestamp(
         $baseDate, $now, $lastOccurence):DateTimeImmutable;
 
